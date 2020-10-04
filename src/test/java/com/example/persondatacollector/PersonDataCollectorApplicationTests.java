025ade0e7e61faaba2c3d4637868e03999b2398a
Stanislav Matšel <stanislav.machel@gmail.com>
2020-10-04 21:45:03 +0300

    Init project

---
+++ src/test/java/com/example/persondatacollector/PersonDataCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.persondatacollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonDataCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
